e349c4de9c9d67156bb0279976d5a1f82532d47c
EWanjau <wamwanjau@gmail.com>
2024-04-27 09:14:04 +0300

    corrected the nginx config file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
 #this bash script sets up the webservers for deployment
-sudo apt-get update
-sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
 sudo ufw allow 'Nginx HTTP'
-sudo service nginx start
-sudo mkdir data/web_static/shared/
-sudo mkdir data/web_static/releases/test/
-sudo echo "<html><head></head><body>ALX School</body></html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s /home/ubuntu/data/web_static/releases/test/ /home/ubuntu/data/web_static/current
-sudo chown -R ubuntu:ubuntu data/
-sudo sed -i '57 i \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo systemctl reload nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<html>
+    <head>
+        <title>Make it to Server 2</title>
+    </head>
+    <body>
+        <p>Installation Complete</p>
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+if [ -L /data/web_static/current ]; then
+    sudo rm /data/web_static/current
+fi
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo rm -f /etc/nginx/sites-enabled/default
+config_text="server {
+    listen 80;
+    listen [::]:80;
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html;
+    }
+}"
+echo "$config_text" | sudo tee /etc/nginx/sites-available/hbnb_static > /dev/null
+if [ ! -e /etc/nginx/sites-enabled/hbnb_static ]; then
+    sudo ln -s /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+fi
+sudo service nginx restart
